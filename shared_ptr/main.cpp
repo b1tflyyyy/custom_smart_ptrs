6380bf5a4b5e28c89f39392554cae7eb93833f17
Myxa <thisisandrew0110@gmail.com>
2023-10-26 10:11:44 -0500

    added new sample

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-#include "custom_allocator.hpp"
+#include "stack_allocator.hpp"
+#include "shared_ptr.hpp"
+
+// TODO: add logs in Makefile
+//
 
 int main()
 {
-	custom_allocator<int, 20> allocator;
-	auto ptr = allocator.allocate(0);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr(new int(5));
+	std::cout << "\n*ptr.get() = " << *ptr.get() << '\n';
+	
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr1(ptr);
+	std::cout << "*ptr1.get() = " << *ptr1.get() << "\n\n"; 
 
-	*ptr = 12;
-	std::cout << *ptr << '\n';
-
-	auto ptr1 = allocator.allocate(0);
-	*ptr1 = 22;
-
-	std::cout <<  *ptr1 << '\n'; 
-
-
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr100(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr2(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr3(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr4(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr5(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr6(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr7(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr8(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr9(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr10(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr11(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr12(ptr);
+	custom::shared_ptr<int, stack_allocator<void, 30>> ptr13(ptr);
+	
 	return 0;
 }
