3d11c8b3c2dd62e9cba30862dadf997e4a32ceb2
Myxa <thisisandrew0110@gmail.com>
2023-11-02 14:30:08 -0500

    added get_memory_used()

--- shared_ptr/stack_allocator.hpp
+++ shared_ptr/stack_allocator.hpp
@@ -3,6 +3,8 @@
 #ifndef STACK_ALLOCATOR_HPP
 #define STACK_ALLOCATOR_HPP
 
+#define ON_STACK_ALLOCATOR_LOGS 1
+
 #include <cstdint>
 #include <stdio.h>
 #include <stdexcept>
@@ -50,8 +52,9 @@ class stack_allocator
                 throw std::runtime_error("Used all memory in allocator");
             }
 
+#if ON_STACK_ALLOCATOR_LOGS
             printf("allocation call: address = %p, size = %zu\n", m_memory_ptr, n);	
-			
+#endif
             m_memory_used += n;
 
             m_current_ptr = m_memory_ptr;
@@ -67,29 +70,42 @@ class stack_allocator
         template <typename U, typename... Args>
         void construct(U* ptr, Args&&... args)
         {
+#if ON_STACK_ALLOCATOR_LOGS
             printf("custom construct\n");
+#endif
             ::new(static_cast<pointer>(ptr)) U(std::forward<Args>(args)...);
         }	
 
         template <typename U, typename... Args>
         constexpr U* construct_at(byte* storage, Args&&... args)
         {
+#if ON_STACK_ALLOCATOR_LOGS
             printf("custom construct_at\n");
+#endif
             return ::new(static_cast<pointer>(storage)) U(std::forward<Args>(args)...);
         }
         
         template <typename U>
         constexpr void destroy_at(U* ptr)
         {
+#if ON_STACK_ALLOCATOR_LOGS
             printf("custom destroy_at\n");
+#endif
             ptr->~U();
         }
 
         template <typename U>
         void destroy(U* ptr)
         {
+#if ON_STACK_ALLOCATOR_LOGS
             printf("custom destroy\n");
+#endif
             ptr->~U();
+        }
+
+        const size_type get_memory_used() const 
+        {
+            return m_memory_used;
         }
 };
 
