8e1557eba4e62d81eba7b0d5c5457b55aec2f4c7
Myxa <thisisandrew0110@gmail.com>
2023-11-20 13:44:28 -0600

    added basic tests for shared_ptr array

--- shared_ptr/tests/shared_ptr_test.cpp
+++ shared_ptr/tests/shared_ptr_test.cpp
@@ -3,6 +3,7 @@
 #include <shared_ptr.hpp>
 #include <memory>
 
+// ===================================================== DEFAULT SHARED PTR =====================================================
 TEST(SharedPtrTests, SharedPtrTests_DEFAULT_MAKE_SHARED_WITH_PARAMS)
 {
     auto ptr = custom::sta::make_shared<int>(777);
@@ -19,6 +20,14 @@ TEST(SharedPtrTests, SharedPtrTests_ALLOC_MAKE_SHARED_WITH_PARAMS)
     const auto expected = 777;
 
     ASSERT_EQ(*ptr, expected);
+}
+
+TEST(SharedPtrTests, SharedPtrTests_DEFAULT_CTOR)
+{
+    custom::shared_ptr<int> ptr(new int(7));
+    *ptr;
+
+    ASSERT_NE(ptr.get(), nullptr);
 }
 
 TEST(SharedPtrTests, SharedPtrTests_ON_COPY)
@@ -48,3 +57,64 @@ TEST(SharedPtrTests, SharedPtrTests_ON_MOVE_VALUE)
 
     ASSERT_EQ(*ptr1, expected);
 }
+// ==========================================================================================================
+
+
+// ===================================================== ARRAY SHARED PTR =====================================================
+#if 0
+// TODO: add make_shared version for array
+TEST(SharedPtrTests, SharedPtrTests_ARRAY_DEFAULT_MAKE_SHARED_WITH_PARAMS)
+{
+    auto ptr = custom::sta::make_shared<int>(777);
+    const auto expected = 777;
+
+    ASSERT_EQ(*ptr, expected);
+}
+
+TEST(SharedPtrTests, SharedPtrTests_ALLOC_MAKE_SHARED_WITH_PARAMS)
+{
+    std::allocator<std::uint8_t> alloc;
+    auto ptr = custom::alc::make_shared<int, decltype(alloc)>(alloc, 777);
+
+    const auto expected = 777;
+
+    ASSERT_EQ(*ptr, expected);
+}
+#endif
+
+TEST(SharedPtrTests, SharedPtrTests_ARRAY_DEFAULT_CTOR)
+{
+    custom::shared_ptr<int> ptr(new int(7));
+    *ptr;
+
+    ASSERT_NE(ptr.get(), nullptr);
+}
+
+TEST(SharedPtrTests, SharedPtrTests_ARRAY_ON_COPY)
+{
+    custom::shared_ptr<int[]> ptr(new int[10] { 1, 2, 3 });
+    auto ptr1(ptr);
+
+    ASSERT_EQ(ptr.get(), ptr1.get());
+}
+
+TEST(SharedPtrTests, SharedPtrTests_ARRAY_ON_MOVE_ADDRESS)
+{
+    custom::shared_ptr<int[]> ptr(new int[10] {1, 2, 3 });
+    int* copy_ptr = ptr.get();
+
+    auto ptr1(std::move(ptr));
+
+    ASSERT_EQ(copy_ptr, ptr1.get());
+}
+
+TEST(SharedPtrTests, SharedPtrTests_ARRAY_ON_MOVE_VALUE)
+{
+    custom::shared_ptr<int[]> ptr(new int[10] {1, 2, 3 });
+    auto ptr1(std::move(ptr));
+
+    const auto expected = 1;
+
+    ASSERT_EQ(*ptr1, expected);
+}
+// ==========================================================================================================
