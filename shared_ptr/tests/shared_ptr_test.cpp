fe4debbb368e1f23763bf7386156824023c82260
Myxa <thisisandrew0110@gmail.com>
2023-12-08 07:11:17 -0600

    updated tests

--- shared_ptr/tests/shared_ptr_test.cpp
+++ shared_ptr/tests/shared_ptr_test.cpp
@@ -3,121 +3,42 @@
 #include <shared_ptr.hpp>
 #include <memory>
 
-// ===================================================== DEFAULT SHARED PTR =====================================================
-TEST(SharedPtrTests, SharedPtrTests_DEFAULT_MAKE_SHARED_WITH_PARAMS)
+// ============================================= DEFAULT SHARED PTR =============================================
+TEST(SharedPtrTests, SharedPtrTests_COPY)
 {
-    auto ptr = custom::sta::make_shared<int>(777);
-    const auto expected = 777;
-
-    ASSERT_EQ(*ptr, expected);
-}
-
-TEST(SharedPtrTests, SharedPtrTests_ALLOC_MAKE_SHARED_WITH_PARAMS)
-{
-    std::allocator<std::uint8_t> alloc;
-    auto ptr = custom::alc::make_shared<int, decltype(alloc)>(alloc, 777);
-
-    const auto expected = 777;
-
-    ASSERT_EQ(*ptr, expected);
-}
-
-TEST(SharedPtrTests, SharedPtrTests_DEFAULT_CTOR)
-{
-    custom::shared_ptr<int> ptr(new int(7));
-    *ptr;
-
-    ASSERT_NE(ptr.get(), nullptr);
-}
-
-TEST(SharedPtrTests, SharedPtrTests_ON_COPY)
-{
-    auto ptr = custom::sta::make_shared<int>(5);
+    custom::shared_ptr<int> ptr(new int(555));
     auto ptr1(ptr);
 
+    ASSERT_EQ(*ptr, *ptr1);
     ASSERT_EQ(ptr.get(), ptr1.get());
+    ASSERT_EQ(ptr.use_count(), 2);
 }
 
-TEST(SharedPtrTests, SharedPtrTests_ON_MOVE_ADDRESS)
+TEST(SharedPtrTests, SharedPtrTests_CUSTOM_ALLOC_DELETER)
 {
-    auto ptr = custom::sta::make_shared<int>(777);
-    int* copy_ptr = ptr.get();
+    std::allocator<int> alloc;
+    int* num = alloc.allocate(sizeof(int));
+    *num = 55;
 
-    auto ptr1(std::move(ptr));
+    custom::shared_ptr<int> ptr(num, [&alloc](int* ptr, std::size_t sz) -> void { alloc.deallocate(ptr, sz); });
 
-    ASSERT_EQ(copy_ptr, ptr1.get());
-}
-
-TEST(SharedPtrTests, SharedPtrTests_ON_MOVE_VALUE)
-{
-    auto ptr = custom::sta::make_shared<int>(777);
-    auto ptr1(std::move(ptr));
-
-    const auto expected = 777;
-
-    ASSERT_EQ(*ptr1, expected);
-}
-// ==========================================================================================================
-
-
-// ===================================================== ARRAY SHARED PTR =====================================================
-// TODO: add make_shared version for array
-TEST(SharedPtrTests, SharedPtrTests_ARRAY_DEFAULT_MAKE_SHARED)
-{
-    std::allocator<std::uint8_t> alloc;
-
-    custom::shared_ptr<int[]> ptr = custom::sta::make_shared<int[]>(6);
-    const auto expected = 777;
-
-    ptr[7] = 777;
-
-    ASSERT_EQ(ptr[7], expected);
-}
-
-TEST(SharedPtrTests, SharedPtrTests_ALLOC_MAKE_SHARED)
-{
-    std::allocator<std::uint8_t> alloc;
-    auto ptr = custom::alc::make_shared<int[], decltype(alloc)>(alloc, 777);
-
-    ptr[6] = 777;
-    const auto expected = 777;
-
-    ASSERT_EQ(ptr[6], expected);
-}
-
-TEST(SharedPtrTests, SharedPtrTests_ARRAY_DEFAULT_CTOR)
-{
-    custom::shared_ptr<int> ptr(new int(7));
-    *ptr;
-
-    ASSERT_NE(ptr.get(), nullptr);
-}
-
-TEST(SharedPtrTests, SharedPtrTests_ARRAY_ON_COPY)
-{
-    custom::shared_ptr<int[]> ptr(new int[10] { 1, 2, 3 });
+    { auto ptr2(ptr); }
     auto ptr1(ptr);
 
+    ASSERT_EQ(*ptr, *ptr1);
     ASSERT_EQ(ptr.get(), ptr1.get());
+    ASSERT_EQ(ptr.use_count(), 2);
 }
 
-TEST(SharedPtrTests, SharedPtrTests_ARRAY_ON_MOVE_ADDRESS)
+TEST(SharedPtrTests, SharedPtrTests_MAKE_SHARED)
 {
-    custom::shared_ptr<int[]> ptr(new int[10] {1, 2, 3 });
-    int* copy_ptr = ptr.get();
+    std::allocator<std::uint8_t> alloc;
 
-    auto ptr1(std::move(ptr));
+    auto ptr = custom::sta::make_shared<int>(55);
+    auto ptr1 = custom::alc::make_shared<int, decltype(alloc)>(alloc, 56);
 
-    ASSERT_EQ(copy_ptr, ptr1.get());
+    ASSERT_EQ(*ptr, 55);
+    ASSERT_EQ(*ptr1, 56);
 }
 
-TEST(SharedPtrTests, SharedPtrTests_ARRAY_ON_MOVE_VALUE)
-{
-    custom::shared_ptr<int[]> ptr(new int[10] {1, 2, 3 });
-    auto ptr1(std::move(ptr));
-
-    const auto expected = 1;
-
-    ASSERT_EQ(*ptr1, expected);
-}
-// ==========================================================================================================
+// ============================================= ARRAY SHARED PTR =============================================
