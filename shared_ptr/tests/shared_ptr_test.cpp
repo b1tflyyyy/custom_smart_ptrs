637c5fdaf136b5c3f6039e38e74774a2edd3e9ca
Myxa <thisisandrew0110@gmail.com>
2023-12-10 06:19:14 -0600

    added tests for shared_ptr array

--- shared_ptr/tests/shared_ptr_test.cpp
+++ shared_ptr/tests/shared_ptr_test.cpp
@@ -32,13 +32,57 @@ TEST(SharedPtrTests, SharedPtrTests_CUSTOM_ALLOC_DELETER)
 
 TEST(SharedPtrTests, SharedPtrTests_MAKE_SHARED)
 {
-    std::allocator<std::uint8_t> alloc;
-
     auto ptr = custom::sta::make_shared<int>(55);
-    auto ptr1 = custom::alc::make_shared<int, decltype(alloc)>(alloc, 56);
-
     ASSERT_EQ(*ptr, 55);
-    ASSERT_EQ(*ptr1, 56);
 }
 
 // ============================================= ARRAY SHARED PTR =============================================
+TEST(SharedPtrTests, SharedPtrArrayTests_COPY)
+{
+    custom::shared_ptr<int[]> ptr(new int[5] { 1, 2, 3, 4, 5 });
+    auto ptr1(ptr);
+
+    ASSERT_EQ(*ptr, *ptr1);
+    ASSERT_EQ(ptr[1], ptr[1]);
+    ASSERT_EQ(ptr[2], ptr[2]);
+    ASSERT_EQ(ptr[3], ptr1[3]);
+    ASSERT_EQ(ptr[4], ptr1[4]);
+
+    ASSERT_EQ(ptr.get(), ptr1.get());
+    ASSERT_EQ(ptr.use_count(), 2);
+}
+
+TEST(SharedPtrTests, SharedPtrArrayTests_CUSTOM_ALLOC_DELETER)
+{
+    std::allocator<int> alloc;
+    int* num = alloc.allocate(sizeof(int) * 3);
+
+    num[0] = 55;
+    num[1] = 77;
+    num[2] = 88;
+
+    custom::shared_ptr<int[]> ptr(num, [&alloc](int* ptr) -> void { alloc.deallocate(ptr, sizeof(int) * 3); });
+
+    { auto ptr2(ptr); }
+    auto ptr1(ptr);
+
+    ASSERT_EQ(ptr[0], ptr1[0]);
+    ASSERT_EQ(ptr[1], ptr1[1]);
+    ASSERT_EQ(ptr[2], ptr1[2]);
+
+    ASSERT_EQ(ptr.get(), ptr1.get());
+    ASSERT_EQ(ptr.use_count(), 2);
+}
+
+TEST(SharedPtrTests, SharedPtrArrayTests_MAKE_SHARED)
+{
+    auto ptr = custom::sta::make_shared<int[]>(3);
+
+    ptr[0] = 1;
+    ptr[1] = 2;
+    ptr[2] = 3;
+
+    ASSERT_EQ(ptr[0], 1);
+    ASSERT_EQ(ptr[1], 2);
+    ASSERT_EQ(ptr[2], 3);
+}
